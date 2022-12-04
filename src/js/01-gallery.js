7342e38149bad7efb65c3ab124c00aa0cc558958
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-12-04 15:26:58 +0200

    sketch

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,50 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+console.log(galleryItems);
+
+const divEl = document.querySelector('div.gallery');
+
+const renderList = (images) => 
+    images.reduce((acc, {preview, original, description}) => 
+    acc + `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`, '');
+
+const insertListItems = (string => {
+    divEl.insertAdjacentHTML('afterbegin', string);
+});
+
+insertListItems(renderList(galleryItems));
+
+divEl.addEventListener('click', clickOpenModalHendler);
+
+function clickOpenModalHendler (evt){
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG"){
+        return;
+    }
+
+    const instance = basicLightbox.create(`<div class="modal">
+        <img class="modal-img" src="${evt.target.dataset.source}" alt="Big img" height = "750px"/>
+    </div>`,{
+	    closable: false,
+        onShow: (instance) => { 
+            document.addEventListener('keydown', clickCloseModalHendler ); 
+        },
+        onClose: (instance) => {
+            document.removeEventListener('keydown', clickCloseModalHendler); 
+        },
+    });
+    instance.show();
+
+    const imageEl = document.querySelector('.modal-img');
+    imageEl.addEventListener ('click', clickCloseModalHendler); 
+    
+    function clickCloseModalHendler (evt){
+        if (evt.code === 'Escape' || evt.target.nodeName === "IMG"){
+           instance.close(); 
+        }     
+    }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+//const throttle = require('lodash.throttle');
+
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+const KEY = "feedback-form-state";
+const formData = {};
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+
+onGetLocalStorage();
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    console.log("formData", formData);
+    const formDataJson = JSON.stringify(formData);
+    localStorage.setItem(KEY, formDataJson);
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log("ВІДПРАВКА ФОРМИ");
+    form.reset();
+    localStorage.removeItem(KEY);
+    console.log("formData", formData);
+}
+
+function onGetLocalStorage() {
+    const savedObj = JSON.parse(localStorage.getItem(KEY));
+    console.log("savedObj", savedObj);
+    if (savedObj) {
+        input.value = savedObj.email;
+        textarea.value = savedObj.message;
+    }
+}
 
