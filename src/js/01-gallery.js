0e7b2bd992edfcc7368b1c9faed618088beee772
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-12-10 00:00:02 +0200

    task  01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,18 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-
 
 console.log(galleryItems);
-
-const divEl = document.querySelector('div.gallery');
 
 const renderList = (images) => 
     images.reduce((acc, {preview, original, description}) => 
-    acc + `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`, '');
+    acc + `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`, '');
+
+const divEl = document.querySelector('div.gallery');
 
 const insertListItems = (string => {
     divEl.insertAdjacentHTML('afterbegin', string);
@@ -19,34 +20,10 @@ const insertListItems = (string => {
 
 insertListItems(renderList(galleryItems));
 
-divEl.addEventListener('click', clickOpenModalHendler);
-
-function clickOpenModalHendler (evt){
-    evt.preventDefault();
-
-    if (evt.target.nodeName !== "IMG"){
-        return;
-    }
-
-    const instance = basicLightbox.create(`<div class="modal">
-        <img class="modal-img" src="${evt.target.dataset.source}" alt="Big img" height = "750px"/>
-    </div>`,{
-	    closable: false,
-        onShow: (instance) => { 
-            document.addEventListener('keydown', clickCloseModalHendler ); 
-        },
-        onClose: (instance) => {
-            document.removeEventListener('keydown', clickCloseModalHendler); 
-        },
-    });
-    instance.show();
-
-    const imageEl = document.querySelector('.modal-img');
-    imageEl.addEventListener ('click', clickCloseModalHendler); 
-    
-    function clickCloseModalHendler (evt){
-        if (evt.code === 'Escape' || evt.target.nodeName === "IMG"){
-           instance.close(); 
-        }     
-    }
-}
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    widthRatio: 0.95,
+    heightRatio: 0.95,
+});
