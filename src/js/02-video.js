56c3485afcc1ff99f85b4d7ec65900abce07925c
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-12-10 16:13:48 +0200

    make some  in task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const KEY = "videoplayer-current-time";
+let data = {};
+
+
+const player = new Player(iframe);
+player.on('play', function() {
+    console.log('start!');
+});
+
+player.on('timeupdate', throttle(setItemToLocalStorage, 1000));
+
+function setItemToLocalStorage (data) {
+    console.log("Записані дані:", data);
+    const dataJson = JSON.stringify(data);
+    console.log("json дані:", dataJson);
+    localStorage.setItem(KEY, dataJson);
+}
+
+//player.setCurrentTime(number)
